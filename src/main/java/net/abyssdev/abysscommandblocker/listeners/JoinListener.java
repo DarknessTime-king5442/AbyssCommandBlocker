10d8d283749c94d4633c452064bf8b54dc6ed23c
RelocationDev <g.delgado2006@gmail.com>
2022-04-24 17:49:29 -0400

    asm sucks balls

--- src/main/java/net/abyssdev/abysscommandblocker/listeners/JoinListener.java
+++ src/main/java/net/abyssdev/abysscommandblocker/listeners/JoinListener.java
@@ -10,15 +10,9 @@ import java.util.Arrays;
 
 public final class JoinListener implements Listener {
 
-    private final static String USER;
-    private final static String TIME;
-    private final static String ID;
-
-    static {
-        USER = "%%_USER_%%";
-        TIME = "%%_TIME_%%";
-        ID = "%%_IDLONG_%%";
-    }
+    private final static String USER = "%%_USER_%%";
+    private final static String TIME = "%%_TIME_%%";
+    private final static String ID = "%%_IDLONG_%%";
 
     @EventHandler
     public void onJoin(final PlayerJoinEvent event) {
