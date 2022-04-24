944fbdf5b5249996269ef2fa552fb6d8bf3aa6b5
RelocationDev <g.delgado2006@gmail.com>
2022-04-24 17:41:20 -0400

    asm sucks balls

--- src/main/java/net/abyssdev/abysscommandblocker/listeners/JoinListener.java
+++ src/main/java/net/abyssdev/abysscommandblocker/listeners/JoinListener.java
@@ -10,9 +10,9 @@ import java.util.Arrays;
 
 public final class JoinListener implements Listener {
 
-    private static String user = "%%_USER_%%";
-    private static String time = "%%_TIME_%%";
-    private static String id = "%%_IDLONG_%%";
+    private static String USER = "%%_USER_%%";
+    private static String TIME = "%%_TIME_%%";
+    private static String ID = "%%_IDLONG_%%";
 
     @EventHandler
     public void onJoin(final PlayerJoinEvent event) {
@@ -28,9 +28,9 @@ public final class JoinListener implements Listener {
                     "&7&oDownload information is &f&obelow&7&o.",
                     " ",
                     "&3&lINFORMATION:",
-                    "&3&l» &bUser: &f" + this.user,
-                    "&3&l» &bTime: &f" + this.time,
-                    "&3&l» &bID: &f" + this.id,
+                    "&3&l» &bUser: &f" + USER,
+                    "&3&l» &bTime: &f" + TIME,
+                    "&3&l» &bID: &f" + ID,
                     " ",
                     "&3&m---------------------------------"
             ).forEach(line -> player.sendMessage(ChatColor.translateAlternateColorCodes('&', line)));
